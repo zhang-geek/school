1ffe71bf619c943162c40c52fb56a8f8a49491e8
cyy <ys18235572343@163.com>
2020-03-06 17:54:55 +0800

    提交pojo

---
+++ src/main/java/com/dj/ssm/pojo/Shop.java
@@ -0,0 +1,26 @@
+package com.dj.ssm.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@TableName("shop")
+public class Shop {
+
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    private String shopName;
+
+    private Double shopPrice;
+
+    private Integer shopStatus;
+
+    private Integer isDel;
+
+    private Integer baseDateId;
+}
